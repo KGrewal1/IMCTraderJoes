9133ce3f6224f31fd488a3348f3e8977ac00f114
kp6169 <45569241+kp6169@users.noreply.github.com>
2023-03-24 14:29:51 +0000

    added new data

--- algoPCC.py
+++ algoPCC.py
@@ -40,6 +40,7 @@ class Asset:
             self.bid_prices.append(bid_price)
 
 
+
 class Trader:
     """
     The trader class, containing a run method which runs the trading algo
@@ -54,6 +55,9 @@ class Trader:
             }
         self.asset_dicts = assets
         self.printing = printing
+        self.last_trade_time = 0
+        self.last_trade = (0,0) # tuple of PC, C :UNSIGNED
+        self.last_trade_volume = (0,0) # tuple of PC, C :SIGNED
     def run(self, state: TradingState) -> Dict[str, List[Order]]:
         """
         Only method required. It takes all buy and sell orders for all symbols as an input,
@@ -69,6 +73,9 @@ class Trader:
 
             if product == 'PINA_COLADAS':
                 # Initialize the list of Orders to be sent as an empty list
+                last_trade_time = self.last_trade_time
+                last_trade_PC, last_trade_C = self.last_trade
+                last_trade_volume_PC, last_trade_volume_C = self.last_trade_volume
                 orders_PC: list[Order] = []
                 limit_PC = assets['PINA_COLADAS'].limit
 
@@ -119,62 +126,179 @@ class Trader:
 
                 mid_C = (best_ask_C+best_bid_C)/2
 
+                profit = 0
+                # buying at the ask price
+                if last_trade_volume_PC<0:
+                    profit = profit -last_trade_volume_PC*last_trade_PC +last_trade_volume_PC*best_ask_PC
 
-                # calculate whether to make a hedge or to go directional
-                if len(assets[product].ask_prices)>=assets[product].period:
-                    fast_avg_PC= (sum(assets['PINA_COLADAS'].ask_prices[-assets['PINA_COLADAS'].fast_period:])+sum(assets['PINA_COLADAS'].bid_prices[-assets['PINA_COLADAS'].fast_period:]))/(2*assets['PINA_COLADAS'].fast_period)
-                    fast_avg_C= (sum(assets['COCONUTS'].ask_prices[-assets['COCONUTS'].fast_period:])+sum(assets['COCONUTS'].bid_prices[-assets['COCONUTS'].fast_period:]))/(2*assets['COCONUTS'].fast_period)
+                # selling at the bid price
+                if last_trade_volume_PC>0:
+                    profit = profit +last_trade_volume_PC*best_bid_PC -last_trade_PC*last_trade_volume_PC
 
-                    #see if the ask of PC moves below the MA
-                    if fast_avg_PC > best_ask_PC:
-                        # see if the ask of C moves below the MA in which case intepret as a down trend
-                        if fast_avg_C > best_ask_C:
-                            order_size_PC = min(best_bid_volume_PC, limit_PC+position_PC)
-                            order_size_C = min(best_bid_volume_C, limit_C+position_C)
+                # buying at the ask price
+                if last_trade_volume_C<0:
+                    profit = profit -last_trade_C*last_trade_volume_C +last_trade_volume_C*best_ask_C
 
+                # selling at the bid price
+                if last_trade_volume_C>0:
+                    profit = profit +last_trade_volume_C*best_bid_C -last_trade_C*last_trade_volume_C
+
+                profit_thresh = 100
+                loss_thresh = 100
+                if position_PC + position_C ==0: out_position = True
+                else: out_position = False
+
+                if profit>profit_thresh:
+                    print('start profit liquidation')
+                    # print('Last Trade for Pina Coladas (P,V):', last_trade_PC, last_trade_volume_PC)
+                    # print('Last Trade for Coconuts (P,V):', last_trade_C, last_trade_volume_C)
+                    # sell if positive position
+                    if position_PC>0:
+                        order_size_PC = min(best_bid_volume_PC, 0+position_PC)
+                        print("SELL", 'PINA_COLADAS', str(order_size_PC) + "x", best_bid_PC)
+                        orders_PC.append(Order('PINA_COLADAS', best_bid_PC, -order_size_PC))
+                        position_PC = position_PC-order_size_PC
+                    # buy if negative position
+                    if position_PC<0:
+                        order_size_PC = min(-best_ask_volume_PC, 0-position_PC)
+                        print("BUY", 'PINA_COLADAS', str(order_size_PC) + "x", best_ask_PC)
+                        orders_PC.append(Order('PINA_COLADAS', best_ask_PC, order_size_PC))
+                        position_PC = position_PC+order_size_PC
+                    # sell if posititive position
+                    if position_C>0:
+                        order_size_C = min(best_bid_volume_C, 0+position_C)
+                        print("SELL", 'COCONUTS', str(order_size_C) + "x", best_bid_C)
+                        orders_C.append(Order('COCONUTS', best_bid_C, -order_size_C))
+                        position_C = position_C-order_size_C
+
+                    # buy if negative position
+                    if position_C<0:
+                        order_size_C = min(-best_ask_volume_C, 0-position_C)
+                        print("BUY", 'COCONUTS', str(order_size_C) + "x", best_ask_C)
+                        orders_C.append(Order('COCONUTS', best_ask_C, order_size_C))
+                        position_C = position_C+order_size_C
+                    self.last_trade_time =0
+
+                    self.last_trade_volume = (position_PC,position_C)
+                    print('end profit liquidation')
+
+
+                    if profit<-loss_thresh:
+                        print('start loss liquidation')
+                        # print('Last Trade for Pina Coladas (P,V):', last_trade_PC, last_trade_volume_PC)
+                        # print('Last Trade for Coconuts (P,V):', last_trade_C, last_trade_volume_C)
+                        # sell if positive position
+                        if position_PC>0:
+                            order_size_PC = min(best_bid_volume_PC, 0+position_PC)
                             print("SELL", 'PINA_COLADAS', str(order_size_PC) + "x", best_bid_PC)
                             orders_PC.append(Order('PINA_COLADAS', best_bid_PC, -order_size_PC))
-
+                            position_PC = position_PC-order_size_PC
+                        # buy if negative position
+                        if position_PC<0:
+                            order_size_PC = min(-best_ask_volume_PC, 0-position_PC)
+                            print("BUY", 'PINA_COLADAS', str(order_size_PC) + "x", best_ask_PC)
+                            orders_PC.append(Order('PINA_COLADAS', best_ask_PC, order_size_PC))
+                            position_PC = position_PC+order_size_PC
+                        # sell if posititive position
+                        if position_C>0:
+                            order_size_C = min(best_bid_volume_C, 0+position_C)
                             print("SELL", 'COCONUTS', str(order_size_C) + "x", best_bid_C)
                             orders_C.append(Order('COCONUTS', best_bid_C, -order_size_C))
+                            position_C = position_C-order_size_C
 
-                        # if both cross in opposite directions assume they are going to normalise and hedge
-                        # we want to go to 0 position
-                        if fast_avg_C < best_bid_C:
-                            order_size_PC = min(-best_ask_volume_PC, limit_PC-position_PC)
-                            order_size_C = min(-best_ask_volume_C, limit_C-position_C)
+                        # buy if negative position
+                        if position_C<0:
+                            order_size_C = min(-best_ask_volume_C, 0-position_C)
+                            print("BUY", 'COCONUTS', str(order_size_C) + "x", best_ask_C)
+                            orders_C.append(Order('COCONUTS', best_ask_C, order_size_C))
+                            position_C = position_C+order_size_C
+                        self.last_trade_time =0
 
-                            print("BUY", 'PINA_COLADAS', str(order_size_PC) + "x", best_bid_PC)
-                            orders_PC.append(Order('PINA_COLADAS', best_bid_PC, order_size_PC))
+                        self.last_trade_volume = (position_PC,position_C)
+                        print('end loss liquidation')
 
-                            # print("BUY", 'COCONUTS', str(order_size_C) + "x", best_bid_C)
-                            # orders_C.append(Order('COCONUTS', best_bid_C, order_size_C))
+                exit_time = 1000
+
+                if last_trade_time>exit_time:
+                    # sell if positive position
+                    if position_PC>0:
+                        order_size_PC = min(best_bid_volume_PC, 0+position_PC)
+                        print("SELL", 'PINA_COLADAS', str(order_size_PC) + "x", best_bid_PC)
+                        orders_PC.append(Order('PINA_COLADAS', best_bid_PC, -order_size_PC))
+                    # buy if negative position
+                    if position_PC<0:
+                        order_size_PC = min(-best_ask_volume_PC, 0-position_PC)
+                        print("BUY", 'PINA_COLADAS', str(order_size_PC) + "x", best_ask_PC)
+                        orders_PC.append(Order('PINA_COLADAS', best_ask_PC, order_size_PC))
+                    # sell if posititive position
+                    if position_C>0:
+                        order_size_C = min(best_bid_volume_C, 0+position_C)
+                        print("SELL", 'COCONUTS', str(order_size_C) + "x", best_bid_C)
+                        orders_C.append(Order('COCONUTS', best_bid_C, -order_size_C))
+
+                    # buy if negative position
+                    if position_C<0:
+                        order_size_C = min(-best_ask_volume_C, 0-position_C)
+                        print("BUY", 'COCONUTS', str(order_size_C) + "x", best_ask_C)
+                        orders_C.append(Order('COCONUTS', best_ask_C, order_size_C))
+
+                    self.last_trade_time =0
 
 
-                    #see if the bid of PC moves above the MA
-                    if fast_avg_PC < best_bid_PC:
-                        # see if the ask of C moves above the MA in which case intepret as an up trend
-                        if fast_avg_C < best_bid_C:
-                            order_size_PC = min(-best_ask_volume_PC, limit_PC-position_PC)
-                            order_size_C = min(-best_ask_volume_C, limit_C-position_C)
 
-                            print("BUY", 'PINA_COLADAS', str(order_size_PC) + "x", best_bid_PC)
-                            orders_PC.append(Order('PINA_COLADAS', best_bid_PC, order_size_PC))
 
-                            print("BUY", 'COCONUTS', str(order_size_C) + "x", best_bid_C)
-                            orders_C.append(Order('COCONUTS', best_bid_C, order_size_C))
+                # calculate whether to make a hedge or to go directional
+                if len(assets[product].ask_prices)>=3 and out_position:
+                    return_PC = ((best_bid_PC+best_ask_PC)/(assets['PINA_COLADAS'].ask_prices[-2]+assets['PINA_COLADAS'].ask_prices[-2])) -1
+                    return_C = ((best_bid_C+best_ask_C)/(assets['COCONUTS'].ask_prices[-2]+assets['COCONUTS'].ask_prices[-2])) -1
 
-                        # if both cross in opposite directions assume they are going to normalise and hedge
-                        # we want to go to 0 position
-                        if fast_avg_C > best_ask_C:
-                            order_size_PC = min(best_bid_volume_PC, limit_PC+position_PC)
-                            order_size_C = min(best_bid_volume_C, limit_C+position_C)
+                    return_delta = return_PC - 0.8*return_C
 
-                            print("SELL", 'PINA_COLADAS', str(order_size_PC) + "x", best_bid_PC)
-                            orders_PC.append(Order('PINA_COLADAS', best_bid_PC, -order_size_PC))
+                    threshold = 0.0003
 
-                            # print("SELL", 'COCONUTS', str(order_size_C) + "x", best_bid_C)
-                            # orders_C.append(Order('COCONUTS', best_bid_C, -order_size_C))
+                    if return_delta > threshold:
+                        order_size_PC = min(best_bid_volume_PC, limit_PC+position_PC)
+                        order_size_C = min(-best_ask_volume_C, limit_C-position_C)
+
+                        if order_size_PC*mid_PC < 0.8*order_size_C*mid_C:
+                            order_size_C = int(np.floor(order_size_PC*mid_PC/(mid_C*0.8)))
+
+                        if order_size_PC*mid_PC > 0.8*order_size_C*mid_C:
+                            order_size_PC = int(np.floor(0.8*order_size_C*mid_C/(mid_PC)))
+
+
+                        print("SELL", 'PINA_COLADAS', str(order_size_PC) + "x", best_bid_PC)
+                        orders_PC.append(Order('PINA_COLADAS', best_bid_PC, -order_size_PC))
+
+
+                        print("BUY", 'COCONUTS', str(order_size_C) + "x", best_ask_C)
+                        orders_C.append(Order('COCONUTS', best_ask_C, order_size_C))
+
+                        self.last_trade_time +=1
+                        self.last_trade = (best_bid_PC,best_ask_C)
+                        self.last_trade_volume = (-order_size_PC,order_size_C)
+
+                    if return_delta < -threshold:
+                        order_size_PC = min(-best_ask_volume_PC, limit_PC-position_PC)
+                        order_size_C = min(best_bid_volume_C, limit_C+position_C)
+
+                        if order_size_PC*mid_PC < 0.8*order_size_C*mid_C:
+                            order_size_C = int(np.floor(order_size_PC*mid_PC/(mid_C*0.8)))
+
+                        if order_size_PC*mid_PC > 0.8*order_size_C*mid_C:
+                            order_size_PC = int(np.floor(0.8*order_size_C*mid_C/(mid_PC)))
+
+                        print("BUY", 'PINA_COLADAS', str(order_size_PC) + "x", best_ask_PC)
+                        orders_PC.append(Order('PINA_COLADAS', best_ask_PC, order_size_PC))
+
+
+                        print("SELL", 'COCONUTS', str(order_size_C) + "x", best_bid_C)
+                        orders_C.append(Order('COCONUTS', best_bid_C, -order_size_C))
+
+                        self.last_trade_time +=1
+                        self.last_trade = (best_ask_PC,best_bid_C)
+                        self.last_trade_volume = (order_size_PC,-order_size_C)
+
 
 
 
